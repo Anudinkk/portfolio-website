711d3c6787ce6b869f69aa8201c8afb2556ef5b7
anudin0899 <anudhinkk@gmail.com>
2024-09-27 16:59:25 +0530

    add a button

--- app/(routes)/works/page.jsx
+++ app/(routes)/works/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='md:px-20 py-5 h-screen'>
+    <div className='px-5 md:px-20 py-5 h-screen'>
       <h2 className='mt-5 text-4xl font-extrabold'>MY WORKS</h2>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
         {PROJECTS.map((item, index) => (
--- app/_components/Work.jsx
+++ app/_components/Work.jsx
@@ -2,19 +2,26 @@
 import React, { useEffect, useState } from 'react'
 import Cards from './Cards'
 import { PROJECTS } from '@/utils/constants'
+import Link from 'next/link'
 
 const Work = () => {
 
   const [Projects, setProjects] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     setProjects(PROJECTS);
-  },[]);
- 
+  }, []);
+
   return (
     <div className='my-10 px-5 md:px-0 lg:px-0'>
-      <div className='px-2'>
-        <h2 className='text-4xl font-extrabold'>MY WORKS</h2>
-        <h4 className='text-lg text-slate-400'>Recent Ones</h4>
+      <div className='px-2 flex justify-between'>
+        <div>
+          <h2 className='text-4xl font-extrabold'>MY WORKS</h2>
+          <h4 className='text-lg text-slate-400'>Recent Ones</h4>
+        </div>
+        <div>
+          <Link href={`/works`}> <h6 className='text-blue-500 cursor-pointer font-semibold'>View All</h6> </Link>
+        </div>
+
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-10'>
         {Projects.map((item, index) => index < 3 && (
